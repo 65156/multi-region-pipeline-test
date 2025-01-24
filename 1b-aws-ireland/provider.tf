67cd9a2b64813bd419cb2b714eb1448622544d38
Frank Carter Smith <frankie@ibm.com>
2025-01-23 19:24:42 -0600

    fix providers

--- 1b-aws-ireland/provider.tf
+++ 1b-aws-ireland/provider.tf
@@ -1,13 +1,5 @@
 terraform {
   required_providers {
-    ibm = {
-      source  = "IBM-Cloud/ibm"
-      version = "1.66.0"
-    }
-  }
-}
 
-provider "ibm" {
-  region           = "us-east"
-  ibmcloud_api_key = var.AUTH_KEY
+  }
 }
