ee82d982d7b1c8631be42f450310183a02f78e99
Frank Carter Smith <frankie@ibm.com>
2025-01-29 08:18:04 -0600

    check

--- 1b-aws-ireland/main.tf
+++ 1b-aws-ireland/main.tf
@@ -1,9 +1,9 @@
-resource "null_resource" "example" {
-  # Using triggers to force execution on every apply
-  triggers = {
-    always_run = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "echo This is a test resource - 1b"
-  }
-}
+# resource "null_resource" "example" {
+#   # Using triggers to force execution on every apply
+#   triggers = {
+#     always_run = timestamp()
+#   }
+#   provisioner "local-exec" {
+#     command = "echo This is a test resource - 1b"
+#   }
+# }
