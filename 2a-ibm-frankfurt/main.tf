7e7a4b1b21e31ac09ec1abdb24b3d0975793d03a
Frank <fraser.carter@gmail.com>
2025-01-29 09:11:38 -0600

    Update main.tf

--- 2a-ibm-frankfurt/main.tf
+++ 2a-ibm-frankfurt/main.tf
@@ -1,12 +1,12 @@
-resource "null_resource" "example" {
-  # Using triggers to force execution on every apply
-  triggers = {
-    always_run = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "echo This is a test resource - 2a"
-  }
-}
+# resource "null_resource" "example" {
+#   # Using triggers to force execution on every apply
+#   triggers = {
+#     always_run = timestamp()
+#   }
+#   provisioner "local-exec" {
+#     command = "echo This is a test resource - 2a"
+#   }
+# }
 
 # resource "ibm_resource_group" "test" {
 #   name     = "test2a"
