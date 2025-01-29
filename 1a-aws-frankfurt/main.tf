3c06748d426530859fa72217d3cc24de39cad288
Frank Carter Smith <frankie@ibm.com>
2025-01-29 08:56:28 -0600

    deploy tests

--- 1a-aws-frankfurt/main.tf
+++ 1a-aws-frankfurt/main.tf
@@ -1,10 +1,10 @@
-resource "null_resource" "example" {
-  # Using triggers to force execution on every apply
-  triggers = {
-    always_run = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "echo This is a test resource - 1a"
-  }
-}
+# resource "null_resource" "example" {
+#   # Using triggers to force execution on every apply
+#   triggers = {
+#     always_run = timestamp()
+#   }
+#   provisioner "local-exec" {
+#     command = "echo This is a test resource - 1a"
+#   }
+# }
 
