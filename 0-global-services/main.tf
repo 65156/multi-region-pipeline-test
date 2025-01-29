3426c2a4aa816842a2cb0d7ae78163e8795a8d10
Frank Carter Smith <frankie@ibm.com>
2025-01-29 08:55:03 -0600

    check dev

--- 0-global-services/main.tf
+++ 0-global-services/main.tf
@@ -1,12 +1,12 @@
-resource "null_resource" "example" {
-  # Using triggers to force execution on every apply
-  triggers = {
-    always_run = timestamp()
-  }
-  provisioner "local-exec" {
-    command = "echo This is a test resource - 0"
-  }
-}
+# resource "null_resource" "example" {
+#   # Using triggers to force execution on every apply
+#   triggers = {
+#     always_run = timestamp()
+#   }
+#   provisioner "local-exec" {
+#     command = "echo This is a test resource - 0"
+#   }
+# }
 
 # resource "ibm_resource_group" "test" {
 #   name     = "test0"
